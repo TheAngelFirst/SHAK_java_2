7477eebc481505c0566d27db323e10deb884d375
TheAngelFirst <angel-first@bk.ru>
2020-01-24 16:07:38 +0300

    Реализовано +,-,/,* дробных чисел

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,5 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author ShiryaevAK
+ */
+
 public class Base {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int numOp = 0;
+        double x1 = 0, x2 = 0, result = 0;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.printf("Введите номер операции (1 - сложение, 2 - вычитание, 3 - деление, 4 - умножение): ");
+            numOp = Integer.parseInt(reader.readLine());
+            System.out.printf("Введите первое число : ");
+            x1 = Double.parseDouble(reader.readLine());
+            System.out.printf("Введите второе число : ");
+            x2 = Double.parseDouble(reader.readLine());
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода: " + e);
+        }
+        switch (numOp) {
+            case (1): //Сложение
+                result = x1 + x2;
+                break;
+            case (2): //Вычетание
+                result = x1 - x2;
+                break;
+            case (3): //Деление
+                result = x1 / x2;
+                break;
+            case (4): //Умножение
+                result = x1 * x2;
+                break;
+            default:
+                break;
+        }
+        System.out.printf("Результат: %.4f", result);
     }
 }
